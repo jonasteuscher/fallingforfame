ceca429810d5c5375ab142830e5efc93aa1ccc30
jonasteuscher <jonas.teuscher@gmail.com>
2026-06-08 15:06:47 +0200

    🐛 Update smooth scrollbehavior

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="h-full antialiased">
+    <html lang="en" data-scroll-behavior="smooth" className="h-full antialiased">
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
